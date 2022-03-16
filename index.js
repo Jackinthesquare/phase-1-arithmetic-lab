e70f2c03edd3cb41bedd2fafdb3d7ca30adb4ae5
Jacky Liang <asheriffy@gmail.com>
2022-03-16 12:14:57 -0400

    commit tree c4b1c01b6594d2a3e384826fcf50b14acb34af33

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+/*const multiply = () => {
+    const num1 = 1;
+    const num2 = 62;
+    return num1*num2;
+    }*/
+
+/*const multiply = (num1,num2) => {
+    if (num1*num2 === 62){
+        return num1*num2
+    }
+}*/
+
+let num1 = 2;
+let num2 = 31
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3%num4;
+
+let max = Math.max(1, 2, 3, 15, 18, 19, 20);
